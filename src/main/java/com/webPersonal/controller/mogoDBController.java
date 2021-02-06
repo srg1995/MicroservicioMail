9ae28dcab30c41aeef93f90fed8b3f6cd9efb135
sergio sacristan santero <srg_ek@hotmail.com>
2021-02-06 22:59:26 +0100

    crossorigin

--- src/main/java/com/webPersonal/controller/mogoDBController.java
+++ src/main/java/com/webPersonal/controller/mogoDBController.java
@@ -8,10 +8,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.webPersonal.dto.MongoDto;
 import com.webPersonal.service.MongoService;
 
+@RestController
 @RequestMapping("/mongoDDBB")
 public class mogoDBController {
 
