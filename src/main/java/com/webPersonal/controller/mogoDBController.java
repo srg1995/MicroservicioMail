253db8634e8c19113ca5bd54a69dca20cd1ffc84
sergio sacristan santero <srg_ek@hotmail.com>
2021-02-06 23:28:15 +0100

    crossorigin

--- src/main/java/com/webPersonal/controller/mogoDBController.java
+++ src/main/java/com/webPersonal/controller/mogoDBController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,7 +24,7 @@ public class mogoDBController {
 	MongoService ms;
 	
 	@CrossOrigin
-	@RequestMapping(value = "/guardar", method = RequestMethod.POST, 
+	@PostMapping(value = "/guardar",
 			consumes = MediaType.APPLICATION_JSON_VALUE, 
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody MongoDto guardar(@RequestBody MongoDto entity) {
@@ -31,7 +32,7 @@ public class mogoDBController {
 	}
 	
 	@CrossOrigin
-	@RequestMapping(value = "/borrar", method = RequestMethod.POST, 
+	@PostMapping(value = "/borrar",
 			consumes = MediaType.APPLICATION_JSON_VALUE, 
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody boolean borrar(@RequestBody MongoDto entity) {
@@ -39,7 +40,7 @@ public class mogoDBController {
 	}
 	
 	@CrossOrigin
-	@RequestMapping(value = "/actualizar", method = RequestMethod.POST, 
+	@PostMapping(value = "/actualizar",
 			consumes = MediaType.APPLICATION_JSON_VALUE, 
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody MongoDto actualizar(@RequestBody MongoDto entity) {
@@ -47,7 +48,7 @@ public class mogoDBController {
 	}
 	
 	@CrossOrigin
-	@RequestMapping(value = "/consultarPorId", method = RequestMethod.POST, 
+	@PostMapping(value = "/consultarPorId", 
 			consumes = MediaType.APPLICATION_JSON_VALUE, 
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody MongoDto consultarPorId(@RequestBody MongoDto entity) {
@@ -55,7 +56,7 @@ public class mogoDBController {
 	}
 	
 	@CrossOrigin
-	@RequestMapping(value = "/consultarTodos", method = RequestMethod.POST, 
+	@PostMapping(value = "/consultarTodos",
 			consumes = MediaType.APPLICATION_JSON_VALUE, 
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody List<MongoDto> consultarTodos() {
